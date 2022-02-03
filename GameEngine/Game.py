3f1986a1e372897d0233ebc1a27e5d8bba1dd32e
Vinh Phat Tu <vphatmail@gmail.com>
2022-02-03 22:49:10 +0000

    changed name

--- GameEngine/Game.py
+++ GameEngine/Game.py
@@ -20,11 +20,23 @@ class Game:
     def menu(self):
         while True:
             #clear_screen()
-            print("Kondoku")
+            print("""
+  .o       .o8                        o.         .o8            oooo                    
+ .8'      "888                        `8.       "888            `888                    
+.8'   .oooo888   .oooo.   ooo. .oo.    `8.  .oooo888   .ooooo.   888  oooo  oooo  oooo  
+88   d88' `888  `P  )88b  `888P"Y88b    88 d88' `888  d88' `88b  888 .8P'   `888  `888  
+88   888   888   .oP"888   888   888    88 888   888  888   888  888888.     888   888  
+`8.  888   888  d8(  888   888   888   .8' 888   888  888   888  888 `88b.   888   888  
+ `8. `Y8bod88P" `Y888""8o o888o o888o .8'  `Y8bod88P" `Y8bod8P' o888o o888o  `V88V"V8P' 
+  `"                                  "'                                               """)
+            print()
+            print("Author: lawclaw")
+            print("-" * 14)
             print("| Easy. 1")
             print("| Medium. 2")
             print("| Hard. 3")
             print("| Quit. 0")
+            print("-" * 14)
             choice = input("Enter: ")
             if choice.isnumeric():
                 if int(choice) in range(0, 4):
