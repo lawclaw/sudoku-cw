381ede7395496aeec2cef869e1213948db7a4af9
Vinh Phat Tu <vphatmail@gmail.com>
2022-02-03 17:36:17 +0000

    difficulty menu

--- GameEngine/Game.py
+++ GameEngine/Game.py
@@ -1,9 +1,23 @@
+def clear_screen():  # https://stackoverflow.com/a/50560686
+    print("\033[H\033[J", end="")
+
 class Game:
     def __init__(self):
-        pass
+        choice = self.menu()
+        if choice == 0:
+            exit()
 
     def menu(self):
-        pass
+        while True:
+            clear_screen()
+            print("Kondoku")
+            print("| Easy. 1")
+            print("| Medium. 2")
+            print("| Hard. 3")
+            print("| Quit. 0")
+            choice = input("Enter: ")
+            if choice.isnumeric():
+                if int(choice) in range(1,4):
+                    return int(choice)
 
-    def clear_screen(self):
-        pass
+
